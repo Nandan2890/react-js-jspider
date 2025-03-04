b8d834de6ec359d651d3c76d26b63f0c6ca2c2f4
Nandan2890 <nkb2890@gmail.com>
2025-03-04 11:08:19 +0530

    cbc

--- myreactapp/src/App.jsx
+++ myreactapp/src/App.jsx
@@ -1,5 +1,6 @@
 //* Parent component
 // import Navbar from "./components/Navbar";
+import Cbc from "./alltopics/typesofcomponents/Cbc";
 import Fbc from "./alltopics/typesofcomponents/fbc";
 
 function App(){
@@ -9,6 +10,7 @@ function App(){
             {/* <Navbar /> */}
 
             <Fbc />
+            <Cbc />
         </div>
 
     );
---
+++ myreactapp/src/alltopics/typesofcomponents/Cbc.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Cbc extends Component{
+    render() {
+        return(
+            <div>
+                <h1>Class based component</h1>
+            </div>
+        );
+    };
+};
+
+export default Cbc;
